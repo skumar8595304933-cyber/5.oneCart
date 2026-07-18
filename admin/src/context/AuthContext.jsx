27348495f4bb9c233ad6e19c850133536316720b
Suraj Kumar <skumar8595304933@gmail.com>
2026-07-18 14:08:53 +0530

    Update AuthContext.jsx

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -1,39 +1,19 @@
-import React, { createContext } from 'react'
+import React, { createContext } from "react";
 
-export const authDataContext = createContext()
-function AuthContext({children}) {
-    let serverUrl = "import React, { createContext } from 'react'
+export const authDataContext = createContext();
 
-export const authDataContext = createContext()
-function AuthContext({children}) {
-    let serverUrl = "https://backend-6ll0.onrender.com"
+function AuthContext({ children }) {
+  const serverUrl = "https://backend-6ll0.onrender.com";
 
-    let value = {
-      serverUrl
-    }
+  const value = {
+    serverUrl,
+  };
+
   return (
-    <div>
-        <authDataContext.Provider value={value}>
-            {children}
-        </authDataContext.Provider>
-      
-    </div>
-  )
+    <authDataContext.Provider value={value}>
+      {children}
+    </authDataContext.Provider>
+  );
 }
 
-export default AuthContext"
-
-    let value = {
-      serverUrl
-    }
-  return (
-    <div>
-        <authDataContext.Provider value={value}>
-            {children}
-        </authDataContext.Provider>
-      
-    </div>
-  )
-}
-
-export default AuthContext
+export default AuthContext;
