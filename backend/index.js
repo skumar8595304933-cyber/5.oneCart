84edc7bba99d5d977c19de0caf9893eae8a40e8e
Suraj Kumar <skumar8595304933@gmail.com>
2026-07-18 13:47:23 +0530

    Update CORS configuration and add routes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,49 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://frontend-4b6l.onrender.com" , "http://localhost:5174"],
+ origin:["https://frontend-4b6l.onrender.com" , "import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/db.js'
+import cookieParser from 'cookie-parser'
+import authRoutes from './routes/authRoutes.js'
+dotenv.config()
+import cors from "cors"
+import userRoutes from './routes/userRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import cartRoutes from './routes/cartRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+
+let port = process.env.PORT || 6000
+
+let app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+ origin:["https://frontend-4b6l.onrender.com" , "https://admin-cmjv.onrender.com"],
+ credentials:true
+}))
+
+app.use("/api/auth",authRoutes)
+app.use("/api/user",userRoutes)
+app.use("/api/product",productRoutes)
+app.use("/api/cart",cartRoutes)
+app.use("/api/order",orderRoutes)
+
+app.get("/", (req, res) => {
+  res.json({
+    status: true,
+    message: "All OK"
+  });
+});
+
+
+app.listen(port,()=>{
+    console.log("Hello From Server")
+    connectDb()
+})
+
+"],
  credentials:true
 }))
 
