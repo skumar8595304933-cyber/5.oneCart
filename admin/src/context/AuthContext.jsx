f7c5fe2bd72081be0d63319c5480b0dbc1e00627
Suraj Kumar <skumar8595304933@gmail.com>
2026-07-18 13:48:45 +0530

    Change server URL in AuthContext
    Updated server URL from localhost to production URL.

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -2,7 +2,26 @@ import React, { createContext } from 'react'
 
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = "import React, { createContext } from 'react'
+
+export const authDataContext = createContext()
+function AuthContext({children}) {
+    let serverUrl = "https://backend-6ll0.onrender.com"
+
+    let value = {
+      serverUrl
+    }
+  return (
+    <div>
+        <authDataContext.Provider value={value}>
+            {children}
+        </authDataContext.Provider>
+      
+    </div>
+  )
+}
+
+export default AuthContext"
 
     let value = {
       serverUrl
