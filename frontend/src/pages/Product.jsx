7794aa0fb9e3e33e633253aa12d69344254ed8a8
Suraj Kumar <skumar8595304933@gmail.com>
2026-07-26 23:37:31 +0530

    Refactor Product component layout and styles
    Updated layout and spacing for the Product page.

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -4,15 +4,14 @@ import BestSeller from '../component/BestSeller'
 
 function Product() {
   return (
-    <div className='w-[100vw] min-h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] flex items-center justify-start flex-col py-[20px]'>
+    <div className='w-full min-h-screen bg-gradient-to-l from-[#141414] to-[#0c2025] flex items-center justify-start flex-col py-[40px] gap-[60px]'>
+      <section className='w-full flex items-center justify-center flex-col gap-[10px]'>
+        <LatestCollection />
+      </section>
 
-        <div className='w-[100%] min-h-[70px] flex items-center justify-center gap-[10px]  flex-col '>
-            <LatestCollection/>
-        </div>
-        <div className='w-[100%] min-h-[70px] flex items-center justify-center gap-[10px]  flex-col '>
-            <BestSeller/>
-        </div>
-      
+      <section className='w-full flex items-center justify-center flex-col gap-[10px]'>
+        <BestSeller />
+      </section>
     </div>
   )
 }
